341faf71434b4ea3e052bab47c046abf35d5c5c5
Christian Neira <caneira.14@est.ucab.edu.ve>
2020-05-08 15:37:50 -0400

    Creada las ruta, el modelo y el controlador de la tabla OFFER

--- modules/offer/offer.model.js
+++ modules/offer/offer.model.js
@@ -1,1 +1,40 @@
-module.exports = {};
+module.exports = {
+/* --------------------------- GET ------------------------- */
+
+  getAllOffers: (con) => {
+    return con.query('SELECT * FROM OFFER').catch((error) => {
+      return new Error(error);
+    });
+  },
+
+  getOffer: (con,o_id) => {
+  	return con.query('SELECT * FROM OFFER WHERE o_id = $1',[o_id]).catch((error) => {
+      return new Error(error);
+    });
+  },
+
+/* ------------------------- POST -------------------------- */
+
+  postOffer: (con,offer) => {
+  	return con.query('INSERT INTO OFFER(o_name,o_valid_through,o_percentage) VALUES($1,$2,$3)',
+  	[offer.o_name,offer.o_valid_through,offer.o_percentage]).catch((error) => {
+      return new Error(error);
+    });
+  },
+
+/* -------------------------- PUT ---------------------------- */
+
+  putOffer: (con,o_id,offer) => {
+  	return con.query('UPDATE OFFER SET o_name = $1,o_valid_through = $2,o_percentage = $3 WHERE o_id = $4',
+  	[offer.o_name,offer.o_valid_through,offer.o_percentage,o_id]).catch((error) => {
+      return new Error(error);
+    });
+  },
+/* ------------------------- DELETE -------------------------- */
+
+  deleteOffer: (con,o_id) => {
+  	return con.query('DELETE FROM OFFER WHERE o_id = $1',[o_id]).catch((error) => {
+      return new Error(error);
+    });
+  },
+};
--- modules/offer/offer.router.js
+++ modules/offer/offer.router.js
@@ -3,4 +3,10 @@ const router = express.Router();
 const offerController = require("./offer.controller");
 const auth = require("../../middlewares/auth");
 
+router.get("/all", /*auth.validateToken ,*/ offerController.getAllOffers);
+router.get("/:id", /*auth.validateToken ,*/ offerController.getOffer);
+router.post("/create", /*auth.validateToken ,*/ offerController.postOffer);
+router.put("/update/:id", /*auth.validateToken ,*/ offerController.putOffer);
+router.delete("/delete/:id", /*auth.validateToken ,*/ offerController.deleteOffer);
+
 module.exports = router;
