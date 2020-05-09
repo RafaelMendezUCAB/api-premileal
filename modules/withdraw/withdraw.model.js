3722adc88ede15a64557009b0475335a610a0bce
Christian Neira <caneira.14@est.ucab.edu.ve>
2020-05-08 23:41:34 -0400

    Creada las rutas, el modelo y el controlodor de la tabla WITHDRAW

--- modules/withdraw/withdraw.model.js
+++ modules/withdraw/withdraw.model.js
@@ -1,1 +1,40 @@
-module.exports = {};
+module.exports = {
+/* --------------------------- GET ------------------------- */
+
+  getAllWithdraws: (con) => {
+    return con.query('SELECT * FROM WITHDRAW').catch((error) => {
+      return new Error(error);
+    });
+  },
+
+  getWithdraw: (con,w_id) => {
+  	return con.query('SELECT * FROM WITHDRAW WHERE w_id = $1',[w_id]).catch((error) => {
+      return new Error(error);
+    });
+  },
+
+/* ------------------------- POST -------------------------- */
+
+  postWithdraw: (con,withdraw) => {
+  	return con.query('INSERT INTO WITHDRAW(w_points,w_dolars,u_w_id,ba_w_id) VALUES($1,$2,$3,$4)',
+  	[withdraw.w_points,withdraw.w_dolars,withdraw.u_w_id,withdraw.ba_w_id]).catch((error) => {
+      return new Error(error);
+    });
+  },
+
+/* -------------------------- PUT ---------------------------- */
+
+  putWithdraw: (con,w_id,withdraw) => {
+  	return con.query('UPDATE WITHDRAW SET w_points = $1, w_dolars = $2, u_w_id = $3, ba_w_id = $4 WHERE w_id = $5',
+  	[withdraw.w_points,withdraw.w_dolars,withdraw.u_w_id,withdraw.ba_w_id,w_id]).catch((error) => {
+      return new Error(error);
+    });
+  },
+/* ------------------------- DELETE -------------------------- */
+
+  deleteWithdraw: (con,w_id) => {
+  	return con.query('DELETE FROM WITHDRAW WHERE w_id = $1',[w_id]).catch((error) => {
+      return new Error(error);
+    });
+  },
+};
--- modules/withdraw/withdraw.router.js
+++ modules/withdraw/withdraw.router.js
@@ -3,4 +3,10 @@ const router = express.Router();
 const withdrawController = require("./withdraw.controller");
 const auth = require("../../middlewares/auth");
 
+router.get("/all", /*auth.validateToken ,*/ withdrawController.getAllWithdraws);
+router.get("/:id", /*auth.validateToken ,*/ withdrawController.getWithdraw);
+router.post("/create", /*auth.validateToken ,*/ withdrawController.postWithdraw);
+router.put("/update/:id", /*auth.validateToken ,*/ withdrawController.putWithdraw);
+router.delete("/delete/:id", /*auth.validateToken ,*/ withdrawController.deleteWithdraw);
+
 module.exports = router;
