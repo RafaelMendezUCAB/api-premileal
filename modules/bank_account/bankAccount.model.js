ea43436eb5b476e7e2d5173911e909fd713d8901
Christian Neira <caneira.14@est.ucab.edu.ve>
2020-05-13 16:55:16 -0400

    FASE1-006: Arreglado errores en algunos querys

--- modules/bank_account/bankAccount.model.js
+++ modules/bank_account/bankAccount.model.js
@@ -15,7 +15,7 @@ module.exports = {
 
 /* ------------------------- POST -------------------------- */
 
-createBankAccount: (con, bankAccount) => {
+  createBankAccount: (con, bankAccount) => {
   	return con.query('INSERT INTO BANK_ACCOUNT(ba_account_type, ba_routing_number, ba_account_number, ba_check_number, ba_is_primary, fk_user_id) VALUES($1,$2,$3,$4,$5,$6)',
   	[bankAccount.account_type, bankAccount.routing_number, bankAccount.account_number, bankAccount.check_number, bankAccount.is_primary, bankAccount.userID]).catch((error) => {
       return new Error(error);
@@ -24,8 +24,8 @@ createBankAccount: (con, bankAccount) => {
 
 /* -------------------------- PUT ---------------------------- */
 
-updateBankAccount: (con, bankAccountID, bankAccount) => {
-  	return con.query('UPDATE BANK_ACCOUNT SET ba_account_type = $1, ba_routing_number = $2, ba_account_number = $3, ba_check_number = $4, ba_is_primary = $5, u_ba_id = $6 WHERE ba_id = $7',
+  updateBankAccount: (con, bankAccountID, bankAccount) => {
+  	return con.query('UPDATE BANK_ACCOUNT SET ba_account_type = $1, ba_routing_number = $2, ba_account_number = $3, ba_check_number = $4, ba_is_primary = $5, fk_user_id = $6 WHERE ba_id = $7',
   	[bankAccount.account_type, bankAccount.routing_number, bankAccount.account_number, bankAccount.check_number, bankAccount.is_primary, bankAccount.userID, bankAccountID]).catch((error) => {
       return new Error(error);
     });
--- modules/user_offer/userOffer.model.js
+++ modules/user_offer/userOffer.model.js
@@ -15,8 +15,8 @@ module.exports = {
 
 /* ------------------------- POST -------------------------- */
 
-createUserOffer: (con, userOffer) => {
-  	return con.query('INSERT INTO USER_OFFER(fk_offer_id, fk_user-id, fk_product_id) VALUES($1,$2,$3)',
+  createUserOffer: (con, userOffer) => {
+  	return con.query('INSERT INTO USER_OFFER(fk_offer_id, fk_user_id, fk_product_id) VALUES($1,$2,$3)',
   	[userOffer.offerID, userOffer.userID, userOffer.productID]).catch((error) => {
       return new Error(error);
     });
@@ -24,7 +24,7 @@ createUserOffer: (con, userOffer) => {
 
 /* -------------------------- PUT ---------------------------- */
 
-updateUserOffer: (con, userOfferID, userOffer) => {
+  updateUserOffer: (con, userOfferID, userOffer) => {
   	return con.query('UPDATE USER_OFFER SET fk_offer_id = $1, fk_user_id = $2, fk_product_id = $3 WHERE uo_id = $4',
   	[userOffer.offerID, userOffer.userID, userOffer.productID, userOfferID]).catch((error) => {
       return new Error(error);
