33808c77bf7d5131683defded6e82431604f0be7
RafaelMendezUCAB <rrmendez.17@est.ucab.edu.ve>
2020-05-23 17:59:17 -0400

    FASE1-009: Registro de cuenta bancaria completo.

--- modules/bank/bank.model.js
+++ modules/bank/bank.model.js
@@ -2,10 +2,18 @@ module.exports = {
 
 /* --------------------------- GET ------------------------- */
     getAllBanks: (con) => {
-      return con.query('SELECT * FROM BANK').catch((error) => {
+      return con.query('SELECT ba_id as "bankID", ba_name as bank, fk_place_id as "placeID" FROM BANK').catch((error) => {
+        console.log(error);
         return new Error(error);
       });
     },
+
+    getRoutingNumbers: (con, bank) => {
+      return con.query('SELECT ro_id as "routingNumberID", ro_number as routingNumber FROM ROUTING_NUMBER, BANK WHERE fk_bank_id = ba_id AND ba_name = \''+bank+'\'').catch((error) => {
+        console.log(error);
+        return new Error(error);
+      });
+    }
 
 /* ------------------------- POST -------------------------- */
 
